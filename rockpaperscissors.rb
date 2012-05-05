4e51521b28fb9ae5c94f36267e181d70ce89f585
David Ladowitz <david@aronsontech.com>
2012-05-05 03:13:12 -0700

    Updated app

--- rockpaperscissors.rb
+++ rockpaperscissors.rb
@@ -4,6 +4,8 @@ choices = ["Rock", "Paper", "Scissors"]
 puts "Choose Rock, Paper or Scissors: "
 yourchoice = gets.capitalize.chomp
 
+puts ""
+
 puts "You chose " + yourchoice + "."
 
 puts ""
@@ -17,5 +19,16 @@ puts
 
 
 result = []<< yourchoice << computerchoice
-print result
+#print result
+
+if result == ["Rock", "Scissors"]  || result == ["Paper", "Rock"]  || result == ["Scissors", "Paper"]
+  puts "You win! Man that was close. I was sweating bullets"
+
+elsif result == ["Rock", "Rock"]  || result == ["Paper", "Paper"]  || result == ["Scissors", "Scissors"]
+  puts "It's a tie. Now what to do?"
+
+else
+  puts "You lost. And to a crummy computer no less."
+
+end
 
