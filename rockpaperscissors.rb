9dda54729be2a7a96b6a240bdbc201f2cb339fd8
David Ladowitz <david@aronsontech.com>
2012-05-05 02:35:14 -0700

    First Ruby Program

---
+++ rockpaperscissors.rb
@@ -0,0 +1,10 @@
+
+#choices = ["rock", "paper", "scissors"]
+#computerchoice = choices[rand(3)]
+Puts = "Choose either rock, paper or scissors"
+mychoice = gets
+
+puts mychoice.chomp
+
+#result = []<< mychoice.chomp << computerchoice
+
